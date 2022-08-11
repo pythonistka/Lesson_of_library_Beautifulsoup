99b7c64cf877b8160b3395707e7b6697a84ae89e
Lopatina Darya <baboleh@rambler.ru>
2022-08-12 02:30:25 +0300

    Создали цикл, с помощью которого будем собирать данные о товарах из каждой категории

--- lesson_2/Lesson_2.py
+++ lesson_2/Lesson_2.py
@@ -7,10 +7,10 @@ import json
 #
 # # заголовки (добавляем accept и user-agent, для того, чтобы показать сайту, что мы не бот, а обычный пользователь)
 # # копируем из браузера во вкладке network в любом из get запросов
-# headers = {
-#     "accept": "*/*",
-#     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
-# }
+headers = {
+    "accept": "*/*",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+}
 # # req будет возвращать результат работы метода get
 # req = requests.get(url, headers=headers)
 # src = req.text
@@ -21,23 +21,49 @@ import json
 # with open("index.html", "w", encoding='utf-8-sig') as file:
 #     file.write(src)
 
-# откроем прочитаем наш файл и сохраним код страницы в переменную
-with open("index.html", encoding='utf-8-sig') as file:
-    src = file.read()
+# # откроем прочитаем наш файл и сохраним код страницы в переменную
+# with open("index.html", encoding='utf-8-sig') as file:
+#     src = file.read()
+#
+# # создадим объект soup и передадим в него в качестве параматера нашу переменную src, парсер lxml и приступим к сборке
+# soup = BeautifulSoup(src, "lxml")
+# # у всех ссылок на группы товаров один и тот же класс, по ним мы и соберем ссылки
+# all_products_hrefs = soup.find_all(class_="mzr-tc-group-item-href")
+#
+# # забираем из всей информации наименовние блоков и ссылки на них, к href добавляем доменное имя https://health-diet.ru/
+# all_categories_dict = {}
+# for item in all_products_hrefs:
+#     item_text = item.text
+#     item_href = "https://health-diet.ru/" + item.get("href")
+#
+#     all_categories_dict[item_text] = item_href
+#
+# # сохраним в json файл
+# with open("all_categories_dict.json", "w") as file:
+#     json.dump(all_categories_dict, file, indent=4, ensure_ascii=False)
 
-# создадим объект soup и передадим в него в качестве параматера нашу переменную src, парсер lxml и приступим к сборке
-soup = BeautifulSoup(src, "lxml")
-# у всех ссылок на группы товаров один и тот же класс, по ним мы и соберем ссылки
-all_products_hrefs = soup.find_all(class_="mzr-tc-group-item-href")
+# загрузим файл в пременную all_categories
+with open("all_categories_dict.json") as file:
+    all_categories = json.load(file)
 
-# забираем из всей информации наименовние блоков и ссылки на них, к href добавляем доменное имя https://health-diet.ru/
-all_categories_dict = {}
-for item in all_products_hrefs:
-    item_text = item.text
-    item_href = "https://health-diet.ru/" + item.get("href")
+# создаем цикл, на каждой итерации которого будем заходить на новую страницу категории, собирать с нее данные о всех
+# товарах и химическом составе и записывать всё это в файл
+count = 0
+for category_name, category_href in all_categories.items():
+#    создадим список из символов, которые хотим заменить, прописываем цикл, если один из символов содержится в имени
+#    категории, то заменяем его на нижний пробел
+    if count == 0 or count == 1:
+        rep = [",", " ", "-", "'"]
+        for item in rep:
+            if item in category_name:
+                category_name = category_name.replace(item, "_")
+        # переходим к запросам на странице (раскоментируем заголовки, чтоб снова можно было их использовать), сохраняем в
+    # переменную src результат
+        req = requests.get(url=category_href, headers=headers)
+        src = req.text
+        #  сохраняем страницу под именем категории (для чистоты проекта создадим папку data и все страницы с файлами
+    #  будем сохранять в нее
+        with open(f"data/{count}_{category_name}.html", "w", encoding='utf-8') as file:
+            file.write(src)
 
-    all_categories_dict[item_text] = item_href
-
-# сохраним в json файл
-with open("all_categories_dict.json", "w") as file:
-    json.dump(all_categories_dict, file, indent=4, ensure_ascii=False)
+        count += 1
