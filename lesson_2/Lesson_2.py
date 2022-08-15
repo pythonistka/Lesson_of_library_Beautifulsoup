793b2509b54bd91a73cafddfa0d70a249156cc0a
Lopatina Darya <baboleh@rambler.ru>
2022-08-16 01:53:07 +0300

    Парсер полностью завершен, сохранение информации по категориям товаров происходит в три файла(csv, html, json)

--- lesson_2/Lesson_2.py
+++ lesson_2/Lesson_2.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -49,42 +52,86 @@ with open("all_categories_dict.json") as file:
 
 # создаем цикл, на каждой итерации которого будем заходить на новую страницу категории, собирать с нее данные о всех
 # товарах и химическом составе и записывать всё это в файл
+iteration_count = int(len(all_categories)) - 1
 count = 0
+print(f"Всего итераций: {iteration_count}")
 for category_name, category_href in all_categories.items():
 #    создадим список из символов, которые хотим заменить, прописываем цикл, если один из символов содержится в имени
 #    категории, то заменяем его на нижний пробел
-    if count == 0 or count == 1:
-        rep = [",", " ", "-", "'"]
-        for item in rep:
-            if item in category_name:
-                category_name = category_name.replace(item, "_")
-        # переходим к запросам на странице (раскоментируем заголовки, чтоб снова можно было их использовать), сохраняем в
-    # переменную src результат
-        req = requests.get(url=category_href, headers=headers)
-        src = req.text
-        #  сохраняем страницу под именем категории (для чистоты проекта создадим папку data и все страницы с файлами
-    #  будем сохранять в нее
-        with open(f"data/{count}_{category_name}.html", "w", encoding='utf-8') as file:
-            file.write(src)
+#     if count == 0 or count == 1:
+    rep = [",", " ", "-", "'"]
+    for item in rep:
+        if item in category_name:
+            category_name = category_name.replace(item, "_")
+    # переходим к запросам на странице (раскоментируем заголовки, чтоб снова можно было их использовать), сохраняем в
+# переменную src результат
+    req = requests.get(url=category_href, headers=headers)
+    src = req.text
+    #  сохраняем страницу под именем категории (для чистоты проекта создадим папку data и все страницы с файлами
+#  будем сохранять в нее
+    with open(f"data/{count}_{category_name}.html", "w", encoding='utf-8') as file:
+        file.write(src)
 
-        # собираем данные со страницы(откроем и сохраним код страницы в переменную
-        with open(f"data/{count}_{category_name}.html", encoding='utf-8') as file:
-            src = file.read()
-        # создаем объект soup
-        soup = BeautifulSoup(src, "lxml")
-        #  собираем заголовки в таблице
-        table_head = soup.find(class_="mzr-tc-group-table").find("tr").find_all("th")
-        product = table_head[0].text
-        calories = table_head[1].text
-        proteins = table_head[2].text
-        fats = table_head[3].text
-        carbohydrates = table_head[4].text
+    # собираем данные со страницы(откроем и сохраним код страницы в переменную
+    with open(f"data/{count}_{category_name}.html", encoding='utf-8') as file:
+        src = file.read()
+    # создаем объект soup
+    soup = BeautifulSoup(src, "lxml")
 
-        with open(f"data/{count}_{category_name}.csv", "w", encoding='utf-8') as file:
+    # проверка страницы на наличие таблицы с продуктами
+    alert_block = soup.find(class_="uk-alert-danger")
+    if alert_block is not None:
+        continue
+
+    #  собираем заголовки в таблице
+    table_head = soup.find(class_="mzr-tc-group-table").find("tr").find_all("th")
+    product = table_head[0].text
+    calories = table_head[1].text
+    proteins = table_head[2].text
+    fats = table_head[3].text
+    carbohydrates = table_head[4].text
+
+    with open(f"data/{count}_{category_name}.csv", "w", encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            (
+                product,
+                calories,
+                proteins,
+                fats,
+                carbohydrates
+            )
+        )
+    # собираем данные о продуктах
+    products_data = soup.find(class_="mzr-tc-group-table").find("tbody").find_all("tr")
+
+    product_info = []
+
+    for item in products_data:
+        product_tds = item.find_all("td")
+        title = product_tds[0].find("a").text
+        calories = product_tds[1].text
+        proteins = product_tds[2].text
+        fats = product_tds[3].text
+        carbohydrates = product_tds[4].text
+
+        product_info.append(
+            {
+                "Title": title,
+                "Calories": calories,
+                "Proteins": proteins,
+                "Fats": fats,
+                "Carbohydrates": carbohydrates
+
+            }
+
+        )
+
+        with open(f"data/{count}_{category_name}.csv", "a", encoding='utf-8') as file:
             writer = csv.writer(file)
             writer.writerow(
                 (
-                    product,
+                    title,
                     calories,
                     proteins,
                     fats,
@@ -92,5 +139,17 @@ for category_name, category_href in all_categories.items():
                 )
             )
 
+    with open(f"data/{count}_{category_name}.json", "a", encoding='utf-8') as file:
+        json.dump(product_info, file, indent=4, ensure_ascii=False)
 
-        count += 1
+
+    count += 1
+    print(f"# Итерация {count}. {category_name} записан")
+    iteration_count = iteration_count - 1
+
+    if iteration_count == 0:
+        print("Работа окончена")
+        break
+
+    print(f"Осталось итераций: {iteration_count}")
+    time.sleep(random.randrange(2, 4))
